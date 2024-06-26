dd6896a251ac7aabfd034e0cd0276dcf6792f191
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-26 16:32:29 +0200

    [FEAT] started the addEventListener to get the data from the form

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 const calcBtn = document.getElementById('price-calculator');
 const canBtn = document.getElementById('cancel');
 
+calcBtn.addEventListener('click', function () {
+    // RECUPERO I DATI DALLA FORM
+    let fullName = document.getElementById('full-name').value;
+    let km = document.getElementById('km').value;
+    let fDE = document.getElementById('fde').value;
+
+    console.log(fullName)
+    console.log(km)
+    console.log(fDE)
+})
