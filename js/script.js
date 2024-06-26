1eea280b886a9f2f9bcaa0c104b7c47099b419ab
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-26 18:01:02 +0200

    [UPD] added header and created output card made also some minor fix

--- js/script.js
+++ js/script.js
@@ -10,17 +10,59 @@ calcBtn.addEventListener('click', function () {
     // CALCOLO IL PREZZO E LO ATTRIBUISCO AD UNA VARIABILE PRICE
     let price = km * 0.21;
     // DICHIARO LA VARIABILE CHE ANDRO A VISUALIZZARE NELL'OUTPUT E LE ASSEGNO IL PREZZO CALCOLATO IN PRECEDENZA
-    let finalPrice = price
+    let finalPrice = (price).toFixed(2)
     // DICHIARO LA VARIABILE MSG CHE ANDRO A MOSTRARE  NELL'OUTPUT
     let msg = `Il passeggero ${fullName} deve pagare il prezzo intero pari a ${finalPrice}€`   
 
+    // DEFINISCO LA CARROZZA
+    let cabin = Math.floor(Math.random() * 15 +1);
+
+    // DEFINISCO LA CODICE CP
+    let cpCode = Math.floor(Math.random() * 99999 +1);
+
+    // DEFINISCO LA CATEGORIA DEL BIGLIETTO
+    let ticketCategory = 'Biglietto Standard'
     // DEFINISCO LE CONDIZIONI NELLE QUALI APPLICARE LO SCONTO
     if(fDE === 'minorenne'){
-        finalPrice = price * 0.8;
+        finalPrice = (price * 0.8).toFixed(2);
         msg = `Il passeggero ${fullName} ha diritto ad uno sconto del 20%, il prezzo del biglietto è di ${finalPrice}€`
+        ticketCategory = 'Biglietto Junior'
     } else if (fDE === 'senior') {
-        finalPrice = price * 0.6;
+        finalPrice = (price * 0.6).toFixed(2);
         msg = `Il passeggero ${fullName} ha diritto ad uno sconto del 40%, il prezzo del biglietto è di ${finalPrice}€`
+        ticketCategory = 'Biglietto Senior'
     }
-    console.log(msg)
+
+    // DEFINISCO L'OUTPUT DELLA CARD DEL BIGLIETTO 
+    document.getElementById('ticket').innerHTML = `<h2 class="text-center text-white">IL TUO BIGLIETTO</h2>
+                        <div class="card p-3">
+                            <h4>DETTAGLIO PASSEGGERI</h4>
+                            <div class="col-12">
+                                <div class="container-fluid">
+                                    <div class="row align-content-center justify-content-center">
+                                        <div class="col-4 bg-gray ticket-height">
+                                            <h4>NOME PASSEGGERO</h4>
+                                            <p>${fullName}</p>
+                                        </div>
+                                        <div class="col-2 d-flex flex-column justify-content-between custom-border-tb">
+                                            <p class="fw-bold">Offerta</p>
+                                            <p>${ticketCategory}</p>
+                                        </div>
+                                        <div class="col-2 d-flex flex-column justify-content-between custom-border-tb">
+                                            <p class="fw-bold">Carrozza</p>
+                                            <p>${cabin}</p>
+                                        </div>
+                                        <div class="col-2 d-flex flex-column justify-content-between custom-border-tb">
+                                            <p class="fw-bold">Codice CP</p>
+                                            <p>${cpCode}</p>
+                                        </div>
+                                        <div class="col-2 d-flex flex-column justify-content-between custom-border-tb custom-border-end">
+                                            <p class="fw-bold">Costo biglietto</p>
+                                            <p>${finalPrice}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`;
+
 })
