610ab02692b181e4294aabf8a7b2be41335e0019
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-26 16:31:17 +0200

    [UPD] defined const for buttons

---
+++ js/script.js
@@ -0,0 +1,4 @@
+    // RECUPERO IL BOTTONE PER IL CALCOLO DEL PREZZO E IL BOTTONE ANNULLA
+const calcBtn = document.getElementById('price-calculator');
+const canBtn = document.getElementById('cancel');
+
